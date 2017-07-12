a8cefdb9c5d4464aeb0f94fb720b275f1c8545da
Fora <luoxuemai@boqii.com>
2017-07-12 18:14:11 +0800

    Move favorite service to playing-interaction-services

--- src/entities/favorite-entity.js
+++
@@ -1,7 +0,0 @@
-import Entity from 'mostly-entity';
-
-const FavoriteEntity = new Entity('Favorite');
-
-FavoriteEntity.excepts('createdAt', 'updatedAt', 'destroyedAt');
-
-export default FavoriteEntity.asImmutable();
