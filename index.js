51715249d9ffae3f120d937dffd9d94cf71705d3
forafish <fora.luo@gmail.com>
2018-04-26 19:12:38 +0800

    remove time log of esm import

--- index.js
+++ index.js
@@ -1,6 +1,4 @@
 require = require("esm")(module/*, options*/);
-console.time('playing-social-services import');
 module.exports = require('./src/index').default;
 module.exports.entities = require('./src/entities').default;
 module.exports.models = require('./src/models').default;
-console.timeEnd('playing-social-services import');
