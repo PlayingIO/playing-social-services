bd8623e3c4a50e77e614fb3a143f507cb811e5d7
forafish <fora.luo@gmail.com>
2018-05-11 10:56:32 +0800

    rename fp.assign to fp.assignAll

--- src/services/followship/followship.service.js
+++ src/services/followship/followship.service.js
@@ -14,7 +14,7 @@ const defaultOptions = {
 
 export class FollowshipService extends Service {
   constructor (options) {
-    options = fp.assign(defaultOptions, options);
+    options = fp.assignAll(defaultOptions, options);
     super(options);
   }
 
